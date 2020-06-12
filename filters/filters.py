e749be4ad76bad60419f6baa2ffc5587744b8d41
asrom <asromahin@yandex.ru>
2020-06-12 22:02:05 +0700

    add unsharp

--- filters/filters.py
+++ filters/filters.py
@@ -4,6 +4,6 @@ import numpy as np
 
 def unsharp_filter(im, kernel_size=(3,3)):
     kernel = np.ones(kernel_size)
-    kernel[kernel_size[0]//2, kernel_size[1]//2] = kernel_size[0]*kernel_size[1]
+    kernel[kernel_size[0]//2, kernel_size[1]//2] = int(kernel_size[0])*int(kernel_size[1])
     im = cv2.filter2D(im, -1, kernel)
     return im
