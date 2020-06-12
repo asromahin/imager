72f8c8f42d0a9e5b61e86401235b49eb23625d17
asrom <asromahin@yandex.ru>
2020-06-12 22:14:05 +0700

    test

--- filters/filters.py
+++ filters/filters.py
@@ -2,12 +2,12 @@
 import cv2
 import numpy as np
 
-def unsharp_filter(im, kernel_size=[3,3]):
-    kernel = np.ones(kernel_size)*(-1)
-    print(kernel_size)
-    print(kernel_size[0]//2, kernel_size[1]//2)
-    print(int(kernel_size[0])*int(kernel_size[1]))
+def unsharp_filter(im, ksize=(3,3)):
+    kernel = np.ones(ksize)*(-1)
+    print(ksize)
+    print(ksize[0]//2, ksize[1]//2)
+    print(int(ksize[0])*int(ksize[1]))
     print(kernel)
-    kernel[kernel_size[0]//2, kernel_size[1]//2] = int(kernel_size[0])*int(kernel_size[1])
+    kernel[ksize[0]//2, ksize[1]//2] = int(ksize[0])*int(ksize[1])
     im = cv2.filter2D(im, -1, kernel)
     return im
