b10a063b0ce640616b43b13fdf3e7b4513322103
asrom <asromahin@yandex.ru>
2020-06-12 20:06:23 +0700

    add new

--- utils.py
+++ utils.py
@@ -123,6 +123,8 @@ def get_bbox_by_features(points, count_of_bbox, shape):
     for i in range(count_of_bbox):
         mask = np.zeros(shape[:2])
         cur_points = points[kmean_res == i]
+        print(cur_points)
+        print(cur_points.shape)
         mask = cv2.fillPoly(mask, cur_points, -1)
         bbox = get_boxes(mask, type='bbox')
         res_bboxes[i] = bbox
